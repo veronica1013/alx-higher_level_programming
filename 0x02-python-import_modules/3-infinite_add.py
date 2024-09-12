69e836dee8244cd42e27a832e567b02e6bc39ff7
veronica1013 <veronica.isiaho@gmail.com>
2024-09-12 12:57:33 -0700

    Adding a file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 import sys
-if __name__ == '__main__':
-    sys.argv.pop(0)
-    total = 0
-    for number in sys.argv:
-        total += int(number)
-    print("{}".format(total))
+
+def main():
+    # The first argument is the script name, so we start from index 1
+    result = sum(int(arg) for arg in sys.argv[1:])
+    print(result)
+
+# Ensure that the code runs only when the script is executed directly
+if __name__ == "__main__":
+    main()
+
