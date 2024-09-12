e62860150e1f26f8728e72e501c714f9c786a651
veronica1013 <veronica.isiaho@gmail.com>
2024-09-12 12:51:37 -0700

    Adding a file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 import sys
-# prints the summation of all its numeric command line arguments
-if __name__ == "__main__":
-    count = len(sys.argv)
-    count -= 1
-    result = 0
-    if count == 0:
-        print("{}".format(count))
-    else:
-        for i in range(1, len(sys.argv)):
-            result += int(sys.argv[i])
-        print("{}".format(result))
+if __name__ == '__main__':
+    sys.argv.pop(0)
+    total = 0
+    for number in sys.argv:
+        total += int(number)
+    print("{}".format(total))
