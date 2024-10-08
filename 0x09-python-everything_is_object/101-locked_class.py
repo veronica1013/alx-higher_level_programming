08c3b365a81dce5599b30f53eb95de1fc2d944a9
veronica1013 <veronica.isiaho@gmail.com>
2024-10-08 08:03:00 -0700

    Adding a file

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,14 @@
 #!/usr/bin/python3
-class LockedClass:
+"""
+Module for a class that prevents dynamic attributes creation
+
+"""
+
+
+class LockedClass():
+    """Class to prevent dynamic attributes creation"""
     __slots__ = ['first_name']
+
+    def __init__(self):
+        """Init method"""
+        pass
